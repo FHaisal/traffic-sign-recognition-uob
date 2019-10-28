f5575901a7c83c6c8a5a81b38619929cb0c5edd5
Faisal <faisal.hoque@hotmail.com>
2019-10-28 23:49:40 +0000

    Created script for test data

---
+++ Deep Learning/TSRDL_Two.m
@@ -0,0 +1,45 @@
+imdsTrain = imageDatastore('Images', ...
+    'IncludeSubfolders',true,'LabelSource','foldernames');
+
+imdsValidation = imageDatastore('Test Images', ...
+    'IncludeSubfolders',true,'LabelSource','foldernames');
+
+layers = [
+    imageInputLayer([32 32 3])
+    
+    convolution2dLayer(3,8,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+    maxPooling2dLayer(2,'Stride',2)
+    
+    convolution2dLayer(3,16,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+    maxPooling2dLayer(2,'Stride',2)
+    
+    convolution2dLayer(3,32,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+    fullyConnectedLayer(43)
+    softmaxLayer
+    classificationLayer];
+
+% Max epoch is 34
+options = trainingOptions('sgdm', ...
+    'InitialLearnRate',0.01, ...
+    'MaxEpochs',50, ...
+    'Shuffle','every-epoch', ...
+    'ValidationData',imdsValidation, ...
+    'ValidationFrequency',306, ...
+    'Verbose',false, ...
+    'Plots','training-progress');
+
+net = trainNetwork(imdsTrain,layers,options);
+
+YPred = classify(net,imdsValidation);
+YValidation = imdsValidation.Labels;
+
+accuracy = sum(YPred == YValidation)/numel(YValidation);
